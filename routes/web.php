dcf92b39f31e51b0cf0e6fd4105e33bf13cf43ca
hamzajafferdeveloper <hamzajaffer999@gmail.com>
2026-01-02 15:10:26 +0500

    Add Cache and Storage Route in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,39 @@
 <?php
 
 use App\Models\ModelProfiles;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/clear-all-caches', function () {
+    try {
+        Artisan::call('cache:clear');
+        Artisan::call('config:clear');
+        Artisan::call('route:clear');
+        Artisan::call('view:clear');
+
+        return 'All caches have been cleared.';
+    } catch (Exception $e) {
+        return 'Error clearing caches: '.$e->getMessage();
+    }
+});
+
+Route::get('/create-storage-link', function () {
+    try {
+        // Define the path to the storage link
+        $linkPath = public_path('storage');
+
+        // Check if the link exists and remove it if it does
+        if (File::exists($linkPath)) {
+            File::delete($linkPath);
+        }
+
+        // Create a new storage link
+        Artisan::call('storage:link');
+
+        return 'The storage link has been recreated.';
+    } catch (Exception $e) {
+        return 'Error creating storage link: '.$e->getMessage();
+    }
+});
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,7 +57,7 @@ Route::get('/model/{id}', function ($id) {
     return view('frontend.model-profile', compact('model'));
 })->name('frontend.model.profile');
 
-require __DIR__ . '/auth.php';
-require __DIR__ . '/admin.php';
-require __DIR__ . '/recruiter.php';
-require __DIR__ . '/professional.php';
+require __DIR__.'/auth.php';
+require __DIR__.'/admin.php';
+require __DIR__.'/recruiter.php';
+require __DIR__.'/professional.php';
